f586f2d8353d6bfed378ef6879c6aa415fe89ac4
motoki317 <motoki317@gmail.com>
2022-07-23 12:08:05 +0900

    sqlite3 to mysql conversion code

---
+++ webapp/sql/convert_sqlite3_to_sql.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+ISUCON_DB_HOST=${ISUCON_DB_HOST:-127.0.0.1}
+ISUCON_DB_PORT=${ISUCON_DB_PORT:-3306}
+ISUCON_DB_USER=${ISUCON_DB_USER:-root}
+ISUCON_DB_PASSWORD=${ISUCON_DB_PASSWORD:-root}
+ISUCON_DB_NAME=${ISUCON_DB_NAME:-isuports}
+
+for tenant_id in $(ls ../tenant_db/*.db | sed -e "s/^\.\.\/tenant_db\///" -e "s/\.db$//")
+do
+  echo "tenant $tenant_id setup"
+  cat <<EOF > "tenant_init_$tenant_id.sql"
+DROP DATABASE IF EXISTS \`tenant_$tenant_id\`;
+CREATE DATABASE \`tenant_$tenant_id\`;
+USE \`tenant_$tenant_id\`;
+EOF
+  ./sqlite3-to-sql "../tenant_db/$tenant_id.db" > "tenant_data_$tenant_id.sql"
+  cat "tenant_init_$tenant_id.sql" tenant/10_schema.sql "tenant_data_$tenant_id.sql" > "../tenant_db/tenant_$tenant_id.sql"
+  rm "tenant_init_$tenant_id.sql" "tenant_data_$tenant_id.sql"
+done
--- webapp/sql/init.sh
+++ webapp/sql/init.sh
@@ -5,8 +5,8 @@ cd `dirname $0`
 
 ISUCON_DB_HOST=${ISUCON_DB_HOST:-127.0.0.1}
 ISUCON_DB_PORT=${ISUCON_DB_PORT:-3306}
-ISUCON_DB_USER=${ISUCON_DB_USER:-isucon}
-ISUCON_DB_PASSWORD=${ISUCON_DB_PASSWORD:-isucon}
+ISUCON_DB_USER=${ISUCON_DB_USER:-root}
+ISUCON_DB_PASSWORD=${ISUCON_DB_PASSWORD:-root}
 ISUCON_DB_NAME=${ISUCON_DB_NAME:-isuports}
 
 # MySQLを初期化
@@ -16,6 +16,22 @@ mysql -u"$ISUCON_DB_USER" \
 		--port "$ISUCON_DB_PORT" \
 		"$ISUCON_DB_NAME" < init.sql
 
+for tenant_id in `ls ../tenant_db/*.db | sed -e "s/^\.\.\/tenant_db\///" -e "s/\.db$//"`
+do
+  echo "tenant $tenant_id setup"
+  cat <<EOF > "tenant_init_$tenant_id.sql"
+USE \`tenant_$tenant_id\`;
+DELETE FROM competition WHERE created_at >= '1654041600';
+DELETE FROM player WHERE created_at >= '1654041600';
+DELETE FROM player_score WHERE created_at >= '1654041600';
+EOF
+  mysql -u"$ISUCON_DB_USER" \
+  		-p"$ISUCON_DB_PASSWORD" \
+  		--host "$ISUCON_DB_HOST" \
+  		--port "$ISUCON_DB_PORT" < "tenant_init_$tenant_id.sql"
+  rm "tenant_init_$tenant_id.sql"
+done
+
 # SQLiteのデータベースを初期化
-rm -f ../tenant_db/*.db
-cp -r ../../initial_data/*.db ../tenant_db/
+# rm -f ../tenant_db/*.db
+# cp -r ../../initial_data/*.db ../tenant_db/

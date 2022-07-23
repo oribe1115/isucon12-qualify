bfe02b356013c0d357339673b3e44c2234ee63d3
motoki317 <motoki317@gmail.com>
2022-07-23 15:25:18 +0900

    add index player_score_tenant_id_competition_id

--- webapp/sql/tenant/10_schema.sql
+++ webapp/sql/tenant/10_schema.sql
@@ -28,5 +28,6 @@ CREATE TABLE player_score (
   score BIGINT NOT NULL,
   row_num BIGINT NOT NULL,
   created_at BIGINT NOT NULL,
-  updated_at BIGINT NOT NULL
+  updated_at BIGINT NOT NULL,
+  KEY `player_score_tenant_id_competition_id` (`tenant_id`, `competition_id`)
 );

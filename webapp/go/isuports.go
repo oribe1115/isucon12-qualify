7d9dc3910f7772fe28879a2213742a79971c71ca
Nagarei <Nagarei@users.noreply.github.com>
2022-07-23 19:35:59 +0900

    Revert "Merge branch 'insert_if'"
    This reverts commit 97e110a646648a65bf3ef7a0ad955bc132e29df0, reversing
    changes made to b10a35fe99dcb4b59ed706abbf71e2921461dbf4.

--- webapp/go/isuports.go
+++ webapp/go/isuports.go
@@ -1459,10 +1459,8 @@ func competitionRankingHandler(c echo.Context) error {
 
 	if _, err := adminDB.ExecContext(
 		ctx,
-		"INSERT INTO visit_history (player_id, tenant_id, competition_id, created_at, updated_at) VALUES (?, ?, ?, ?, ?)"+
-			" where not exists tenant_id=? AND competition_id=? AND player_id=?",
+		"INSERT INTO visit_history (player_id, tenant_id, competition_id, created_at, updated_at) VALUES (?, ?, ?, ?, ?)",
 		v.playerID, tenant.ID, competitionID, now, now,
-		v.playerID, tenant.ID, competitionID,
 	); err != nil {
 		return fmt.Errorf(
 			"error Insert visit_history: playerID=%s, tenantID=%d, competitionID=%s, createdAt=%d, updatedAt=%d, %w",

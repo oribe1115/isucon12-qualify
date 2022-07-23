46ade8abc017301cfb936f8bccc339dd1b95574e
motoki317 <motoki317@gmail.com>
2022-07-23 14:58:15 +0900

    use root in init.sh

--- webapp/sql/insert_sqlite3_to_sql.sh
+++ webapp/sql/insert_sqlite3_to_sql.sh
@@ -2,8 +2,8 @@
 
 ISUCON_DB_HOST=${ISUCON_DB_HOST:-127.0.0.1}
 ISUCON_DB_PORT=${ISUCON_DB_PORT:-3306}
-ISUCON_DB_USER=${ISUCON_DB_USER:-root}
-ISUCON_DB_PASSWORD=${ISUCON_DB_PASSWORD:-root}
+ISUCON_DB_USER=root
+ISUCON_DB_PASSWORD=root
 ISUCON_DB_NAME=${ISUCON_DB_NAME:-isuports}
 
 for tenant_id in $(ls ../../initial_data/*.db | sed -e "s/^\.\.\/\.\.\/initial_data\///" -e "s/\.db$//")

dc0dfab77d0d16c25e261633a2ebb441e326c4b0
motoki317 <motoki317@gmail.com>
2022-07-23 13:15:06 +0900

    bulk insert

--- webapp/sql/convert_sqlite3_to_sql.sh
+++ webapp/sql/convert_sqlite3_to_sql.sh
@@ -14,7 +14,8 @@ DROP DATABASE IF EXISTS \`tenant_$tenant_id\`;
 CREATE DATABASE \`tenant_$tenant_id\`;
 USE \`tenant_$tenant_id\`;
 EOF
-  ./sqlite3-to-sql "../tenant_db/$tenant_id.db" > "tenant_data_$tenant_id.sql"
+  ./sqlite3-to-sql "../tenant_db/$tenant_id.db" > "tenant_data_tmp_$tenant_id.sql"
+  sed -e ':a;N;$!ba;s/;\nINSERT INTO competition VALUES/,/g' -e ':a;N;$!ba;s/;\nINSERT INTO player VALUES/,/g' -e ':a;N;$!ba;s/;\nINSERT INTO player_score VALUES/,/g' "tenant_data_tmp$tenant_id.sql" > "tenant_data_$tenant_id.sql"
   cat "tenant_init_$tenant_id.sql" tenant/10_schema.sql "tenant_data_$tenant_id.sql" > "../tenant_db/tenant_$tenant_id.sql"
-  rm "tenant_init_$tenant_id.sql" "tenant_data_$tenant_id.sql"
+  rm "tenant_init_$tenant_id.sql" "tenant_data_$tenant_id.sql" "tenant_data_tmp_$tenant_id.sql"
 done
